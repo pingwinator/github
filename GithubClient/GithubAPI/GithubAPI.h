4abca501419f3220c5244a9fc94666da23871176
Vasyl Liutikov <pingwinator@gmail.com>
2018-02-05 23:39:19 +0100

    added base project

---
+++ GithubClient/GithubAPI/GithubAPI.h
@@ -0,0 +1,19 @@
+//
+//  GithubAPI.h
+//  GithubAPI
+//
+//  Created by Vasyl Liutikov on 05.02.2018.
+//  Copyright © 2018 pingwinator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GithubAPI.
+FOUNDATION_EXPORT double GithubAPIVersionNumber;
+
+//! Project version string for GithubAPI.
+FOUNDATION_EXPORT const unsigned char GithubAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GithubAPI/PublicHeader.h>
+
+
